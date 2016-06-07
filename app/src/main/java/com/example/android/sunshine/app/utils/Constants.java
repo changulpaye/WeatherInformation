b8f4918bd63fa8482f3403b89a68debe770538fc
Parmeshwar Changulpaye -X (pchangul - ZENSAR TECHNOLOGIES INC at Cisco) <pchangul@partnet.cisco.com>
2016-06-07 13:08:21 +0530

    First Commit

---
+++ app/src/main/java/com/example/android/sunshine/app/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.app.utils;
+
+/**
+ * Created by pchangul on 6/3/2016.
+ */
+public class Constants {
+
+
+    public static final String OPEN_WEATHER_MAP_API_KEY = "514700d44223c1e418709b9887f9bdd4";
+    public static final String QUERY_PARAM = "q";
+    public static final String FORMAT_PARAM = "mode";
+    public static final String UNITS_PARAM = "units";
+    public static final String DAYS_PARAM = "cnt";
+    public static final String APPID_PARAM = "APPID";
+}
